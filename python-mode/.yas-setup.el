286a802d3ab4102164dc9cbc45829a873635e7db
James Ferguson <jferguson@gnubio.com>
2013-12-11 08:38:42 -0500

    align '--' in Python docstring arg list
    Tweak to function recently added python-args-to-docstring.
    
    Now it will look like this:
    
    def my_func(foo, barbar=0, bazbazbaz = False)
        """
    
        Keyword Arguments:
        foo       --
        barbar    -- (default 0)
        bazbazbaz -- (default False)
    
        """

--- python-mode/.yas-setup.el
+++ python-mode/.yas-setup.el
@@ -7,11 +7,13 @@
 (defun python-args-to-docstring ()
   "return docstring format for the python arguments in yas-text"
   (let* ((indent (concat "\n" (make-string (current-column) 32)))
-         (args (mapconcat
+         (args (python-split-args yas-text))
+         (max-len (if args (apply 'max (mapcar '(lambda (x) (length (nth 0 x))) args)) 0))
+         (formatted-args (mapconcat
                 '(lambda (x)
-                   (concat (nth 0 x) " -- "
+                   (concat (nth 0 x) (make-string (- max-len (length (nth 0 x))) ? ) " -- "
                            (if (nth 1 x) (concat "\(default " (nth 1 x) "\)"))))
-                (python-split-args yas-text)
+                args
                 indent)))
-    (unless (string= args "")
-      (mapconcat 'identity (list "Keyword Arguments:" args) indent))))
+    (unless (string= formatted-args "")
+      (mapconcat 'identity (list "Keyword Arguments:" formatted-args) indent))))
