06ea85a06e9daf81ec97f9e70ec93a89f545684a
Andrea Crotti <andrea.crotti.0@gmail.com>
2014-01-28 15:24:31 +0000

    add missing function for the bang snippet, fixes #17

--- text-mode/.yas-setup.el
+++ text-mode/.yas-setup.el
@@ -1,2 +1,11 @@
 (defun yas-with-comment (str)
   (format "%s%s%s" comment-start str comment-end))
+
+(defun ca-all-asscs (asslist query)
+  "returns a list of all corresponding values (like rassoc)"
+  (cond
+   ((null asslist) nil)
+   (t
+    (if (equal (cdr (car asslist)) query)
+        (cons (car (car asslist)) (ca-all-asscs (cdr asslist) query))
+      (ca-all-asscs (cdr asslist) query)))))
