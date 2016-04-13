e2409dd612e22497f50677aad6cda4e50e1aa7ea
sh-ow <sh-ow@users.noreply.github.com>
2016-04-14 00:59:31 +0200

    prog-mode: rename yas-get-comment-xxx functions, different approach for whitespace trim

--- prog-mode/.yas-setup.el
+++ prog-mode/.yas-setup.el
@@ -1,23 +1,37 @@
 (defun yas-with-comment (str)
   (format "%s%s%s" comment-start str comment-end))
 
+;; whitespace removing functions from Magnar Sveen ;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+(defun yas-s-trim-left (s)
+  "Remove whitespace at the beginning of S."
+  (if (string-match "\\`[ \t\n\r]+" s)
+      (replace-match "" t t s)
+    s))
+
+(defun yas-s-trim-right (s)
+	   "Remove whitespace at the end of S."
+	   (if (string-match "[ \t\n\r]+\\'" s)
+		   (replace-match "" t t s)
+		 s))
+
+(defun yas-s-trim (s)
+  "Remove whitespace at the beginning and end of S."
+  (yas-s-trim-left (yas-s-trim-right s)))
+
+
 (defun yas-string-reverse (str)
+  "Reverse a string STR manually to be compatible with emacs versions < 25."
   (apply #'string
 	     (reverse
 	      (string-to-list str))))
 
-(defun yas-get-comment-start ()
-  (substring comment-start 0
-			 (dotimes (i (length comment-start))
-			   (unless (or (eq (aref (yas-string-reverse comment-start) i) nil)
-						   (eq (aref (yas-string-reverse comment-start) i) 32))
-				 (return (- (length comment-start) i))))))
+(defun yas-trimmed-comment-start ()
+  "This function returns `comment-start' trimmed by whitespaces."
+  (yas-s-trim comment-start))
 
-(defun yas-get-comment-end ()
+(defun yas-trimmed-comment-end ()
+  "This function returns `comment-end' trimmed by whitespaces if `comment-end' is not empty.
+Otherwise the reversed output of function `yas-trimmed-comment-start' is returned."
   (if (eq (length comment-end) 0)
-	  (yas-string-reverse (yas-get-comment-start))
-	(substring comment-end
-			   (dotimes (i (length comment-end))
-				 (unless (or (eq (aref comment-end i) nil)
-							 (eq (aref comment-end i) 32))
-				   (return i))))))
+	  (yas-string-reverse (yas-trimmed-comment-start))
+	(yas-s-trim comment-end)))
