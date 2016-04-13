9b30fe4123b1b2b14f4234a5c4bf2ef5301904e0
sh-ow <sh-ow@users.noreply.github.com>
2016-04-14 00:14:47 +0200

    don't use reverse function on string

--- prog-mode/.yas-setup.el
+++ prog-mode/.yas-setup.el
@@ -1,16 +1,21 @@
 (defun yas-with-comment (str)
   (format "%s%s%s" comment-start str comment-end))
 
+(defun yas-string-reverse (str)
+  (apply #'string
+	     (reverse
+	      (string-to-list str))))
+
 (defun yas-get-comment-start ()
   (substring comment-start 0
 			 (dotimes (i (length comment-start))
-			   (unless (or (eq (aref (reverse comment-start) i) nil)
-						   (eq (aref (reverse comment-start) i) 32))
+			   (unless (or (eq (aref (yas-string-reverse comment-start) i) nil)
+						   (eq (aref (yas-string-reverse comment-start) i) 32))
 				 (return (- (length comment-start) i))))))
 
 (defun yas-get-comment-end ()
   (if (eq (length comment-end) 0)
-	  (reverse (yas-get-comment-start))
+	  (yas-string-reverse (yas-get-comment-start))
 	(substring comment-end
 			   (dotimes (i (length comment-end))
 				 (unless (or (eq (aref comment-end i) nil)
