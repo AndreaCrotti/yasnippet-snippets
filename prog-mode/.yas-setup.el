29e0a872dec37cff3c3e8325056e5fc7020e4562
sh-ow <sh-ow@users.noreply.github.com>
2016-04-13 22:53:48 +0200

    refactor code, put some code as functions in .yas-setup.el

--- prog-mode/.yas-setup.el
+++ prog-mode/.yas-setup.el
@@ -1,2 +1,18 @@
 (defun yas-with-comment (str)
   (format "%s%s%s" comment-start str comment-end))
+
+(defun yas-get-comment-start ()
+  (substring comment-start 0
+			 (dotimes (i (length comment-start))
+			   (unless (or (eq (aref (reverse comment-start) i) nil)
+						   (eq (aref (reverse comment-start) i) 32))
+				 (return (- (length comment-start) i))))))
+
+(defun yas-get-comment-end ()
+  (if (eq (length comment-end) 0)
+	  (reverse start)
+	(substring comment-end
+			   (dotimes (i (length comment-end))
+				 (unless (or (eq (aref comment-end i) nil)
+							 (eq (aref comment-end i) 32))
+				   (return i))))))
