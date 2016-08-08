63e65c22d2fe3d080c2a6bee65e55615c15cf782
Andrea Crotti <andrea.crotti.0@gmail.com>
2016-08-08 14:26:54 +0100

    remove unused file

--- fundamental-mode/.yas-setup.el
+++
@@ -1,8 +0,0 @@
-(defun ca-all-asscs (asslist query)
-  "returns a list of all corresponding values (like rassoc)"
-  (cond
-   ((null asslist) nil)
-   (t
-    (if (equal (cdr (car asslist)) query)
-        (cons (car (car asslist)) (ca-all-asscs (cdr asslist) query))
-      (ca-all-asscs (cdr asslist) query)))))
