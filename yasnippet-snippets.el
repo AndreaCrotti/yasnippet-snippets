6ff001decd016fa753392192936204e020ea3785
Andrea Crotti <andrea.crotti.0@gmail.com>
2017-08-21 11:02:44 +0100

    update version

--- yasnippet-snippets.el
+++ yasnippet-snippets.el
@@ -4,7 +4,7 @@
 
 ;; Author: Andrea Crotti <andrea.crotti.0@gmail.com>
 ;; Keywords: snippets
-;; Version: 1.0.1
+;; Version: 1.0.2
 ;; Package-Requires: ((yasnippet "0.8.0"))
 
 ;; This program is free software; you can redistribute it and/or modify
