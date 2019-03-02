8dcde0fe4ecdb2757c8cf9db919b36eb5f9edeef
Andrea Crotti <andrea.crotti.0@gmail.com>
2019-03-02 10:06:53 +0000

    add initial reporting project

---
+++ report/project.clj
@@ -0,0 +1,9 @@
+(defproject report "0.1.0-SNAPSHOT"
+  :description "Give a report of the yas snippets"
+  :dependencies [[org.clojure/clojure "1.10.0"]]
+
+  :uberjar-name "report.jar"
+  :min-lein-version "2.8.1"
+  :source-paths ["src"]
+  :test-paths ["test"]
+  :resource-paths ["resources"])
