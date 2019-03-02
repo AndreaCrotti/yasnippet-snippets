b3db3b4602d0626b66b5c6169340cba1f72ee85c
Andrea Crotti <andrea.crotti.0@gmail.com>
2019-03-02 11:03:27 +0000

    parsing and mode files

--- report/src/core.clj
+++ report/src/core.clj
@@ -1,10 +1,28 @@
 (ns core
-  (:require [clojure.string :as str]))
+  "Parse files and generate some html reports"
+  (:require [clojure.string :as str]
+            [clojure.java.io :as io]))
 
-(defn extract-name
-  [filename]
+(defn kw-pattern
+  [kw]
+  (re-pattern (format "# %s: (\\w+)" kw)))
+
+(defn extract-keyword
+  [filename keyword]
   (let [lines (-> filename
                   slurp
                   str/split-lines)]
     (remove nil?
-            (map #(last (re-find #"# name: (\w+)" %)) lines))))
+            (map #(last (re-find (kw-pattern keyword) %)) lines))))
+
+(defn mode-files
+  [mode-dir]
+  (filter #(.isFile %)
+          (file-seq (io/file mode-dir))))
+
+(defn parse-mode
+  [mode-dir]
+  (for [f (mode-files mode-dir)]
+    {:name (extract-keyword f "name")
+     :key (extract-keyword f "key")
+     :group (extract-keyword f "group")}))
