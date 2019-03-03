01df4244f236f080b126df360e3c4720cd5d7110
Andrea Crotti <andrea.crotti.0@gmail.com>
2019-03-03 16:47:36 +0000

    full initial HTML generation of modes

--- report/project.clj
+++ report/project.clj
@@ -1,6 +1,7 @@
 (defproject report "0.1.0-SNAPSHOT"
   :description "Give a report of the yas snippets"
-  :dependencies [[org.clojure/clojure "1.10.0"]]
+  :dependencies [[org.clojure/clojure "1.10.0"]
+                 [hiccup "1.0.5"]]
 
   :uberjar-name "report.jar"
   :min-lein-version "2.8.1"
--- report/src/core.clj
+++ report/src/core.clj
@@ -1,7 +1,11 @@
 (ns core
   "Parse files and generate some html reports"
   (:require [clojure.string :as str]
+            [clojure.edn :as edn]
+            [hiccup.core :as hiccup]
             [clojure.java.io :as io]))
+
+;; TODO: do something whenever the mode has just `.yas-parents` and nothing else?
 
 (defn kw-pattern
   [kw]
@@ -24,11 +28,42 @@
 (defn parse-mode
   [mode-dir]
   (for [f (mode-files mode-dir)]
-    {:name (extract-keyword f "name")
+    {:filename f
+     :name (extract-keyword f "name")
      :key (extract-keyword f "key")
      :group (extract-keyword f "group")}))
 
 (defn parse-everything
   [snippets-dir]
-  (for [d (file-seq (io/file snippets-dir))]
-    {d (parse-mode d)}))
+  (into {}
+        (remove nil?)
+        (for [d (file-seq (io/file snippets-dir))]
+          (when (.isDirectory d)
+            {(.getName d) (parse-mode d)}))))
+
+(defn store-to-edn
+  [snippets-dir output-file]
+  (spit output-file (parse-everything snippets-dir)))
+
+(defn edn->hiccup
+  "Transform the data structure containing all the modes described
+  into an hiccup data structure"
+  [modes]
+  (for [[m snips] (sort-by first modes)]
+    (into [:div.mode (str "Mode: " m)]
+          (for [s snips]
+            [:ul
+             [:li "link =" (:filename s)]
+             [:li "name =" (:name s)]
+             [:li "group =" (:group s)]
+             [:li "key =" (:key s)]]))))
+
+(comment
+  (let [all-modes (parse-everything "../snippets")
+        hicup (edn->hiccup all-modes)
+        content [:html
+                 (into [:body]
+                       hicup)]]
+    (spit
+     "hello.html"
+     (hiccup/html content))))
