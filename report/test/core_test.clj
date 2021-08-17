2d898fc000e531f0ad4d2c5b6d9e8bf48e9eb0d6
Andrea Crotti <andrea.crotti.0@gmail.com>
2021-08-17 21:17:20 +0100

    add test file

---
+++ report/test/core_test.clj
@@ -0,0 +1,9 @@
+(ns core-test
+  (:require [clojure.test :refer [deftest testing is]]
+            [core :as sut]))
+
+(deftest unique-test
+  (let [all-modes (sut/parse-everything "../snippets")]
+    (doseq [[k snippets] all-modes]
+      (let [keys (map :key snippets)]
+        (is (= (count keys) (count (set keys))) (str "failed for mode " k))))))
